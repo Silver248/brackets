e15c7016ecdffbee8dec6cbc4c7ef59a43eacce2
Silver248 <wmanzhela@gmail.com>
2019-10-12 00:49:20 +0300

    feat: add solution for Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+let n = 0;
+let m = 0;
+for (let i=0; i<str.length; i++) {
+    if (i='('||'['){
+        n=n+1;
+    } else if (i=')'||']'){
+        n=n-1;
+    } else if (i='|') {
+        m=m+1;
+    }
+  }
+  if ((n=0) && (m%2 == 0)) {
+      return true;
+  } else {
+      return false;
+  }
 }
